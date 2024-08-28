401d53caf2f4da92c4a89574b5e4b79082a03310
david1312 <davidbernadi13@gmail.com>
2024-08-29 00:58:03 +0700

    remove aria label since already have button text

--- src/components/Section/Experience/Experience.tsx
+++ src/components/Section/Experience/Experience.tsx
@@ -38,7 +38,6 @@ const ExperienceSection: React.FC = () => {
               tabIndex={activeTab === index ? 0 : -1}
               aria-selected={activeTab === index ? true : false}
               aria-controls={`panel-${index}`}
-              aria-label={experience.company}
             >
               {experience.company}
             </TabButton>
