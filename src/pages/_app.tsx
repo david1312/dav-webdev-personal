d9e538f9d88cebc885181d30f646fc592ac96f6e
david1312 <davidbernadi13@gmail.com>
2024-08-28 11:17:14 +0700

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,26 +8,30 @@ import { IsMobileProvider } from "@/context/IsMobileContext";
 import MetaHead from "@/components/MetaHead/MetaHead";
 import SideRight from "@/components/Side/SideRight";
 import SideLeft from "@/components/Side/SideLeft";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [loading, setLoading] = useState<boolean>(true);
 
   return (
-    <IsMobileProvider>
-      <MetaHead />
-      <GlobalStyles />
-      {loading ? (
-        <Loader finishLoading={() => setLoading(false)} />
-      ) : (
-        <>
-          <Header />
-          <SideLeft />
-          <SideRight />
-          <Component {...pageProps} />
-          <Footer />
-        </>
-      )}
-    </IsMobileProvider>
+    <>
+      <IsMobileProvider>
+        <MetaHead />
+        <GlobalStyles />
+        {loading ? (
+          <Loader finishLoading={() => setLoading(false)} />
+        ) : (
+          <>
+            <Header />
+            <SideLeft />
+            <SideRight />
+            <Component {...pageProps} />
+            <Footer />
+          </>
+        )}
+      </IsMobileProvider>
+      <Analytics />
+    </>
   );
 };
 
